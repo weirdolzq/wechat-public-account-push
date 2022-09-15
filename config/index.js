dcde8a2755878f1959b487963eb161ab0e31e93a
weirdolzq <112836025+weirdolzq@users.noreply.github.com>
2022-09-15 15:39:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -51,6 +51,10 @@ export const config = {
       customizedDateList: [
         // 在一起的日子
         {"keyword": "love_day", date: "2020-04-19"},
+        // 结婚纪念日
+        {"keyword": "marry_day", date: "2023-09-09"},
+        // 退伍日
+        {"keyword": "ex_day", date: "2023-09-10"},
       ]
     },
     {
